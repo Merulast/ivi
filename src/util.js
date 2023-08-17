8eba94611dcf6aeaec0302c825da757952ac2079
Merula <86977165+Merulast@users.noreply.github.com>
2023-08-17 14:14:21 +0200

    Update util.js
    sleep promise added

--- src/util.js
+++ src/util.js
@@ -180,3 +180,5 @@ const loadImageCache = () => {
 }
 
 const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
